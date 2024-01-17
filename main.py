52d9f723559362f279067b7f1abbab3b7550ce07
jkarenko <juho.karenko@gmail.com>
2024-01-17 15:31:55 +0200

    handle audio to speech

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from fastapi import FastAPI, UploadFile, File, Request
+import shutil
+from fastapi.templating import Jinja2Templates
 from os import getenv
-from fastapi import FastAPI
 from meeting_notes_ai.media_handler import MediaHandler
 
+templates = Jinja2Templates(directory="templates")
+
 app = FastAPI(
-    root_path=getenv("ROOT_PATH", "http://localhost:8000"),
+    root_path=getenv("ROOT_PATH", "http://127.0.0.1:8000"),
 )
 media_handler = MediaHandler()
 
@@ -26,3 +30,20 @@ def speech_to_text(audio_path: str):
 @app.post("/meeting_minutes")
 def meeting_minutes(transcription: str):
     return media_handler.meeting_minutes(transcription)
+
+
+@app.post("/audio_to_minutes")
+async def audio_to_minutes(request: Request, file: UploadFile = File(...)):
+    with open(f"{file.filename}", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    meeting_summary = media_handler.audio_to_minutes(file.filename)
+    print(meeting_summary)
+    import json
+    with open('meeting_summary.json', 'w') as json_file:
+        json.dump(meeting_summary, json_file)
+    return templates.TemplateResponse("meeting_summary.html", {"request": request, "meeting_summary": meeting_summary})
+
+
+@app.get("/")
+def ui(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
--- meeting_notes_ai/media_handler.py
+++ meeting_notes_ai/media_handler.py
@@ -1,4 +1,7 @@
+import uuid
+import os
 import ffmpeg
+from pathlib import Path
 from openai import OpenAI
 
 client = OpenAI()
@@ -9,36 +12,49 @@ class MediaHandler:
 
 
     def extract_audio(self, input=None, output=None):
-        ffmpeg.input(input).output(output, vn=True, filter='atempo=2.0', ab=64, acodec='libmp3lame').run()
+        print("Extracting audio...", end="")
+        stream = ffmpeg.input(input)
+        stream = ffmpeg.output(stream, output, vn=True, filter='atempo=2.0', ab=64, acodec='libmp3lame')
+        ffmpeg.run(stream, overwrite_output=True)
+        print(" done!")
+        return output
 
 
     def compress_audio(self, input=None, output=None):
-        ffmpeg.input(input).output(output, filter='atempo=2.0', ab=64, acodec='libmp3lame').run()
+        stream = ffmpeg.input(input)
+        stream = ffmpeg.output(stream, output, filter='atempo=2.0', acodec='libmp3lame')
+        ffmpeg.run(stream, overwrite_output=True)
+        return output
 
 
-    def speech_to_text(self, audio_path=None):
-
-        with open(audio_path, "rb") as audio_file:
-            transcript = client.audio.transcriptions.create(
-                model="whisper-1",
-                file=audio_file,
-                response_format="text"
-            )
-        return transcript
+    def speech_to_text(self, audio_path):
+        audio_path_obj = Path(audio_path)
+        output_path = audio_path_obj.stem + "_processed.mp3"
+        output_path = self.compress_audio(audio_path, output_path)
+        with open(output_path, 'rb') as audio_file:
+            print("Transcribing audio...", end="")
+            transcription = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+            print(" done!")
+        return transcription.text
 
 
     def meeting_minutes(self, transcription):
+        print(transcription)
+        print("Extracting meeting summary...", end="")
         abstract_summary = self.abstract_summary_extraction(transcription)
+        print(" done!")
+        print("Extracting action items...", end="")
         action_items = self.action_item_extraction(transcription)
+        print(" done!")
         return {
             'abstract_summary': abstract_summary,
             'action_items': action_items,
         }
 
 
-    def abstract_summary_extraction(transcription):
+    def abstract_summary_extraction(self, transcription):
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-4-1106-preview",
             temperature=0,
             messages=[
                 {
@@ -51,12 +67,12 @@ class MediaHandler:
                 }
             ]
         )
-        return response['choices'][0]['message']['content']
+        return response.choices[0].message.content
 
 
-    def action_item_extraction(transcription):
+    def action_item_extraction(self, transcription):
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-4-1106-preview",
             temperature=0,
             messages=[
                 {
@@ -69,4 +85,9 @@ class MediaHandler:
                 }
             ]
         )
-        return response['choices'][0]['message']['content']
+        return response.choices[0].message.content
+
+
+    def audio_to_minutes(self, audio_file):
+        transcription = self.speech_to_text(audio_file)
+        return self.meeting_minutes(transcription)
